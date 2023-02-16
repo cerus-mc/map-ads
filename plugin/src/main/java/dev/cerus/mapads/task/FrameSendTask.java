d749ff8d6fe55effd93be952831df8a204cc8371
Cerus <github.social@cerus.dev>
2023-02-16 19:26:31 +0100

    Remove weird try/catch

--- plugin/src/main/java/dev/cerus/mapads/task/FrameSendTask.java
+++ plugin/src/main/java/dev/cerus/mapads/task/FrameSendTask.java
@@ -17,12 +17,10 @@ public class FrameSendTask implements Runnable {
     private final double distHard;
     private final double distSoft;
 
-    private final ConfigModel configModel;
     private final AdScreenStorage adScreenStorage;
     private int ticks;
 
     public FrameSendTask(final ConfigModel configModel, final AdScreenStorage adScreenStorage) {
-        this.configModel = configModel;
         this.adScreenStorage = adScreenStorage;
 
         this.distHard = configModel.despawnRange * configModel.despawnRange;
@@ -31,50 +29,46 @@ public class FrameSendTask implements Runnable {
 
     @Override
     public void run() {
-        try {
-            for (final AdScreen screen : this.adScreenStorage.getScreens()) {
-                final MapScreen mapScreen = MapScreenRegistry.getScreen(screen.getScreenId());
-                if (mapScreen != null && mapScreen.getLocation() != null && mapScreen.getLocation().getWorld() != null) {
-                    final Location screenLoc = mapScreen.getLocation();
-                    for (final Player player : ReviewerUtil.getNonReviewingPlayers(mapScreen, false)) {
-                        if (!player.getWorld().getName().equals(screenLoc.getWorld().getName())) {
-                            if (ScreenViewerUtil.isViewer(mapScreen, player)) {
-                                ScreenViewerUtil.removeViewer(mapScreen, player);
-                                if (MapsCompat.isThreeOrAbove()) {
-                                    mapScreen.despawnFrames(player);
-                                }
-                            }
-                            continue;
-                        }
-
-                        final double distance = player.getLocation().distanceSquared(screenLoc);
-                        if (ScreenViewerUtil.isViewer(mapScreen, player) && distance > this.distHard) {
+        for (final AdScreen screen : this.adScreenStorage.getScreens()) {
+            final MapScreen mapScreen = MapScreenRegistry.getScreen(screen.getScreenId());
+            if (mapScreen != null && mapScreen.getLocation() != null && mapScreen.getLocation().getWorld() != null) {
+                final Location screenLoc = mapScreen.getLocation();
+                for (final Player player : ReviewerUtil.getNonReviewingPlayers(mapScreen, false)) {
+                    if (!player.getWorld().getName().equals(screenLoc.getWorld().getName())) {
+                        if (ScreenViewerUtil.isViewer(mapScreen, player)) {
                             ScreenViewerUtil.removeViewer(mapScreen, player);
                             if (MapsCompat.isThreeOrAbove()) {
                                 mapScreen.despawnFrames(player);
                             }
-                        } else if (!ScreenViewerUtil.isViewer(mapScreen, player) && distance < this.distHard) {
-                            ScreenViewerUtil.addViewer(mapScreen, player);
-                            if (MapsCompat.isThreeOrAbove()) {
-                                mapScreen.spawnFrames(player);
-                            } else {
-                                mapScreen.sendFrames(player);
-                            }
-                            mapScreen.sendMaps(true, player);
-                        } else if (distance > this.distSoft && distance < this.distHard) {
+                        }
+                        continue;
+                    }
+
+                    final double distance = player.getLocation().distanceSquared(screenLoc);
+                    if (ScreenViewerUtil.isViewer(mapScreen, player) && distance > this.distHard) {
+                        ScreenViewerUtil.removeViewer(mapScreen, player);
+                        if (MapsCompat.isThreeOrAbove()) {
+                            mapScreen.despawnFrames(player);
+                        }
+                    } else if (!ScreenViewerUtil.isViewer(mapScreen, player) && distance < this.distHard) {
+                        ScreenViewerUtil.addViewer(mapScreen, player);
+                        if (MapsCompat.isThreeOrAbove()) {
+                            mapScreen.spawnFrames(player);
+                        } else {
                             mapScreen.sendFrames(player);
                         }
+                        mapScreen.sendMaps(true, player);
+                    } else if (distance > this.distSoft && distance < this.distHard) {
+                        mapScreen.sendFrames(player);
                     }
                 }
             }
+        }
 
-            if (this.ticks * (TICK_PERIOD / 20) >= 10) {
-                this.ticks = 0;
-            } else {
-                this.ticks++;
-            }
-        } catch (final Throwable t) {
-            System.out.println(t.getMessage());
+        if (this.ticks * (TICK_PERIOD / 20) >= 10) {
+            this.ticks = 0;
+        } else {
+            this.ticks++;
         }
     }
 
